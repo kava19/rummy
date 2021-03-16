b59ce87fa9c7fd186050c928266573a55eae5682
kava19 <adam.pentkowski124@gmail.com>
2021-03-16 20:40:29 +0100

    use ipv4 in test server

--- src/scl/test_server.cpp
+++ src/scl/test_server.cpp
@@ -3,7 +3,7 @@
 int main(){
 	scl::server serv;
 
-	auto ec = scl::server_create(&serv, "::1", 50000, true);
+	auto ec = scl::server_create(&serv, "127.0.0.1", 50000, false);
 	if(ec != scl::err_code::OK)
 		printf("Cannot create server: '%s'\n", scl::err_code_desc[ec].c_str());
 
